5909edc742a4dcc8f02d73c1f106b82923d36940
zgy0813 <158253778@qq.com>
2022-04-09 22:37:08 +0800

    添加MJMH commit

---
+++ JMH/src/main/java/com/zgy/BenchmarkTest.java
@@ -0,0 +1,57 @@
+package com.zgy;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+@BenchmarkMode({Mode.AverageTime, Mode.Throughput, Mode.SampleTime, Mode.SingleShotTime})
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Benchmark)
+public class BenchmarkTest {
+
+    @Param({"10000"})
+    private int length;
+
+    private int[] numbers;
+    private Calculator singleThreadCalc;
+    private Calculator multiThreadCalc;
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(BenchmarkTest.class.getSimpleName())
+                .forks(1)
+                .warmupIterations(5)
+                .measurementIterations(5)
+                .build();
+
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public long singleThreadBench() {
+        return singleThreadCalc.sum(numbers);
+    }
+
+    @Benchmark
+    public long multiThreadBench() {
+        return multiThreadCalc.sum(numbers);
+    }
+
+    @Setup
+    public void prepare() {
+        numbers = IntStream.rangeClosed(1, length).toArray();
+        singleThreadCalc = new SingleThreadCalculator();
+        multiThreadCalc = new MultiThreadCalculator(Runtime.getRuntime().availableProcessors());
+    }
+
+    @TearDown
+    public void shutdown() {
+        singleThreadCalc.shutdown();
+        multiThreadCalc.shutdown();
+    }
+}
---
+++ JMH/src/main/java/com/zgy/SingleThreadCalculator.java
@@ -0,0 +1,16 @@
+package com.zgy;
+
+public class SingleThreadCalculator implements Calculator {
+    public long sum(int[] numbers) {
+        long total = 0L;
+        for (int i : numbers) {
+            total += i;
+        }
+        return total;
+    }
+
+    @Override
+    public void shutdown() {
+        // nothing to do
+    }
+}
